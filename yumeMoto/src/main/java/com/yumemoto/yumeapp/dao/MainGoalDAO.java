8c8432e9cd1113ca169b9c95739777e3fe152f0b
leeyoungseung <rudnfskf2@gmail.com>
2018-05-13 12:14:05 +0900

    2018/05/13 1. 작성자 : Lee 2. 작업내용
     1)DAO Interface
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao
     MainGoalDAO, MemberDAO, SecondGoalDAO, ThirdGoalDAO
     
     - 기본기능 정의(데이터 생성, 데이터 읽기, 데이터 수정, 데이터 삭제, 데이터 목록보기)
     
     2)DAO Class
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao/daoImpl
     MainGoalDAOImpl, MemberDAOImpl, SecondGoalDAOImpl, ThirdGoalDAOImpl
     
     - 기본기능 상세정의 (추가기능은 아직 정의하지 않았음.)
     - DB 연결 객체 의존성 주입 정의
     
     3) Mybatis Mapper.xml
     yumeMoto/src/main/resources/mappers
     mainGoalMapper, memberMapper, secondMapper, thirdMapper
     
     - DB 쿼리 정의
     
     ----------------------------------------------------------
    2018/05/13
    1. Writer : Lee
    2. Working history
    
     1)DAO Interface
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao
     MainGoalDAO, MemberDAO, SecondGoalDAO, ThirdGoalDAO
     
     - Definition of basic function (creating data, updating data, deleting
    data, viewing list of data)
     
     2)DAO Class
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao/daoImpl
     MainGoalDAOImpl, MemberDAOImpl, SecondGoalDAOImpl, ThirdGoalDAOImpl
     
     - Detailed definition of basic function (Didn't define add function
    yet.)
     - Definition of DB connection Object Dependency Injection
     
     3) Mybatis Mapper.xml
     yumeMoto/src/main/resources/mappers
     mainGoalMapper, memberMapper, secondMapper, thirdMapper
     
     - Definition of DB Qurey.
     
    ----------------------------------------------------------
    
    2018/05/13
    1. 作成者 : 李
    2. 作業内容
    
     1)DAO Interface
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao
     MainGoalDAO, MemberDAO, SecondGoalDAO, ThirdGoalDAO
    
     - 基本機能の定義（データの生成、データの読み、データの修正、データの削除、データのリストを見る
     
     2)DAO Class
     yumeMoto/src/main/java/com/yumemoto/yumeapp/dao/daoImpl
     MainGoalDAOImpl, MemberDAOImpl, SecondGoalDAOImpl, ThirdGoalDAOImpl
    
     - 基本機能の詳細定義（追加機能はまだ定義しておりません。）
     - データベースオブジェクトのDIの定義
     
     3) Mybatis Mapper.xml
     yumeMoto/src/main/resources/mappers
     mainGoalMapper, memberMapper, secondMapper, thirdMapper
     
     - DBのクエリーの定義

---
+++ yumeMoto/src/main/java/com/yumemoto/yumeapp/dao/MainGoalDAO.java
@@ -0,0 +1,23 @@
+/**
+ * This Interface is abstract DAO.
+ * It define basic functions which are create, read, update, delete, list..
+ */
+package com.yumemoto.yumeapp.dao;
+
+import java.util.List;
+
+import com.yumemoto.yumeapp.dbDomain.MainGoalVO;
+
+public interface MainGoalDAO {
+
+	public int createMainGoal(MainGoalVO mg) throws Exception;
+	
+	public MainGoalVO readMainGoal(Integer m_num) throws Exception;
+	
+	public int updateMainGoal(MainGoalVO mg) throws Exception;
+	
+	public int deleteMainGoal(Integer m_num) throws Exception;
+	
+	public List<MainGoalVO> listMainGoal() throws Exception;
+	
+}
